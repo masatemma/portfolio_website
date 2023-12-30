84a4b63f67067618dea85d75d8140b3dc5a381df
masatemma <temma.masa@gmail.com>
2023-12-30 15:12:13 +0800

    finished content revealing animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,3 +94,17 @@ const scrollActive = () =>{
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true //Animation repeat
+})
+
+sr.reveal(`.home__data, .home__social, .contact__container, .footer_container`)
+sr.reveal(`.home__image`, {origin: 'bottom'})
+sr.reveal(`.home__data, .skills__data`, {origin: 'left'})
+sr.reveal(`.about__image, .skills__content`, {origin: 'right'})
+sr.reveal(`.projects__card`, {interval: 100})
+
